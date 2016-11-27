7abe51b053f4baaec355c0b20ddcc76f68369afc
Newton Jain <newton.jain@gmail.com>
2016-11-27 15:01:34 -0800

    Adding Glimpse node modules

--- config/env.js
+++ config/env.js
@@ -4,12 +4,12 @@ var rootPath = path.normalize(__dirname + '/../'); // normalizes to base path
 module.exports = {
 	development: {
 		rootPath: rootPath,
-		database: 'mongodb://localhost/express-todo',
+		database: 'mongodb://node-to-do-list:7L20NWjFXvlXxk4pRAoGA0MKkjemeA0vSJEis63XtWuL6n1EX8kDsDqVezezFaDtz7UsAcJWaM458PLODrlJmA==@node-to-do-list.documents.azure.com:10250/?ssl=true',
 		port: process.env.PORT || 3000
 	},
 	production: {
 		rootPath: rootPath,
-		database: 'mongodb://localhost/express-todo',
+		database: 'mongodb://node-to-do-list:7L20NWjFXvlXxk4pRAoGA0MKkjemeA0vSJEis63XtWuL6n1EX8kDsDqVezezFaDtz7UsAcJWaM458PLODrlJmA==@node-to-do-list.documents.azure.com:10250/?ssl=true',
 		port: process.env.PORT || 80
 	}
 };
